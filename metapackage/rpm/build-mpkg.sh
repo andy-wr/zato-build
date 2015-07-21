4c48357444827ec7f2ab1f656bd7691019328fec
Andrzej Wróbel <andy@zato.io>
2015-07-21 21:15:17 +0200

    metapkg rhel7

---
+++ metapackage/rpm/build-mpkg.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [[ -z "$1" ]]
+then
+echo Argument 1 must be Zato version
+    exit 1
+fi
+
+ZATO_VERSION=$1
+
+CURDIR="${BASH_SOURCE[0]}";RL="readlink";([[ `uname -s`=='Darwin' ]] || RL="$RL -f")
+while([ -h "${CURDIR}" ]) do CURDIR=`$RL "${CURDIR}"`; done
+N="/dev/null";pushd .>$N;cd `dirname ${CURDIR}`>$N;CURDIR=`pwd`;popd>$N
+
+SOURCE_DIR=$CURDIR/package-base
+TMP_DIR=/opt/tmp
+RPM_BUILD_DIR=/root/rpmbuild
+
+function prepare {
+  yum install -y rpm-build rpmdevtools wget
+  rpmdev-setuptree
+}
+
+function cleanup {
+    rm -rf $TMP_DIR
+    rm -rf $RPM_BUILD_DIR/BUILDROOT
+}
+
+function build_rpm {
+    rm -f $SOURCE_DIR/zato.spec
+    cp $SOURCE_DIR/zato.spec.template $SOURCE_DIR/zato.spec
+    sed -i.bak "s/ZATO_VERSION/$ZATO_VERSION/g" $SOURCE_DIR/zato.spec
+    mkdir -p $RPM_BUILD_DIR/SPECS/
+    cp $SOURCE_DIR/zato.spec $RPM_BUILD_DIR/SPECS/
+
+    mkdir $RPM_BUILD_DIR/BUILDROOT
+    mkdir $RPM_BUILD_DIR/BUILDROOT/zato-metapackage-$ZATO_VERSION
+    cd $CURDIR
+    cd $RPM_BUILD_DIR/SPECS
+    rpmbuild -ba zato.spec
+}
+
+prepare
+cleanup
+build_rpm
